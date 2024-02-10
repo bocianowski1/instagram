ceafe781dc081f0cbe84311adae791d8d2a48b5f
torgerbocianowski <torgerboc@gmail.com>
2024-02-10 14:52:56 +0100

    update post and user preview

--- client/src/components/post.tsx
+++ client/src/components/post.tsx
@@ -1,20 +1,35 @@
 import { UserPreview } from "./user-preview";
 import { FiHeart, FiMessageCircle } from "react-icons/fi";
+import { FaHeart } from "react-icons/fa";
 import { PiPaperPlaneRight } from "react-icons/pi";
 import { FaRegBookmark } from "react-icons/fa";
-import Image from "next/image";
+import { Post } from "@/lib/types";
+import { getUserByUsername } from "@/api/users";
+import { likePost, postComment } from "@/api/posts";
+import { getAuth } from "@/api/auth";
+import { postLikedByUser } from "@/lib/utils";
+import { LikeButton } from "./like-button";
+import Link from "next/link";
 
-export function Post({ imageURL }: { imageURL: string }) {
+type Props = {
+  post: Post;
+};
+
+export async function PostCard({ post }: Props) {
+  const { user } = await getAuth();
+  const postUser = await getUserByUsername(post.username);
+
   return (
     <div className="w-full flex flex-col h-fit pb-3">
-      <section className="flex gap-3 items-center p-2">
-        {/* <UserPreview
-          user={null}
-        /> */}
-        <p className="font-semibold">faze_torger</p>
+      <section className="flex gap-3 items-center p-3">
+        <UserPreview user={postUser} hasStory />
+        <Link href={`/auth/profile/${post.username}`} className="font-semibold">
+          {post.username} {post.ID}
+        </Link>
       </section>
-      <section className="h-[50vh]">
-        {imageURL.endsWith("mp4") ? (
+      <section className="h-[50vh] w-[600px] px-[1px] bg-neutral-200">
+        <div className="w-full h-full bg-gradient-to-br from-pink-300 to-purple-300" />
+        {/* {imageURL.endsWith("mp4") ? (
           <video
             src={imageURL}
             autoPlay
@@ -30,11 +45,24 @@ export function Post({ imageURL }: { imageURL: string }) {
             width={400}
             className="w-full h-full object-cover"
           />
-        )}
+        )} */}
       </section>
-      <section className="bg-white flex justify-between p-4 text-2xl">
+      <section className="flex justify-between p-4 text-2xl">
         <div className="flex items-center gap-4">
-          <FiHeart />
+          <form
+            action={likePost}
+            method="POST"
+            className="flex items-center justify-center"
+          >
+            <input type="hidden" name="postId" value={post.ID} />
+            <input type="hidden" name="username" value={user.username} />
+            <input
+              type="hidden"
+              name="action"
+              value={postLikedByUser(post, user.username) ? "unlike" : "like"}
+            />
+            <LikeButton post={post} user={user} />
+          </form>
           <FiMessageCircle />
           <div className="-rotate-45">
             <PiPaperPlaneRight />
@@ -43,26 +71,99 @@ export function Post({ imageURL }: { imageURL: string }) {
         <FaRegBookmark />
       </section>
       <section className="flex flex-col gap-2 px-2">
-        <div className="flex">
-          <ul className="flex">
-            <li className="">
-              <div className="bg-red-200 border border-gray-400 rounded-full h-6 w-6" />
-            </li>
-            <li className="-translate-x-2">
-              <div className="bg-blue-200 border border-gray-400 rounded-full h-6 w-6" />
-            </li>
-            <li className="-translate-x-4">
-              <div className="bg-orange-200 border border-gray-400 rounded-full h-6 w-6" />
-            </li>
+        <div className="flex items-center gap-2">
+          <ul className="flex pr-2">
+            {post.likes.length > 0 &&
+              post.likes.map(async (like, idx) => {
+                if (idx === 3) return;
+                const user = await getUserByUsername(like.username);
+                let xOffset = idx * 2;
+                return (
+                  <li key={like.ID} className={`-translate-x-${xOffset}`}>
+                    <UserPreview
+                      user={user}
+                      hideUrl
+                      className="h-8 w-8 p-2 font-normal text-sm border border-gray-500"
+                    />
+                  </li>
+                );
+              })}
           </ul>
-          <p className="-translate-x-2">
-            Liked by <span className="font-semibold">optic_paal2</span> and{" "}
-            <span className="font-semibold">others</span>
-          </p>
+          {post.likes.length > 0 && (
+            <p className="-translate-x-2">
+              Liked by{" "}
+              <span className="font-semibold">{post.likes[0].username}</span>{" "}
+              {post.likes.length === 2 && (
+                <>
+                  <span>and </span>
+                  <span className="font-semibold">
+                    {post.likes[1].username}
+                  </span>
+                </>
+              )}
+              {post.likes.length > 2 && (
+                <>
+                  <span>and </span>
+                  <span className="font-semibold">
+                    {post.likes.length - 1} others
+                  </span>
+                </>
+              )}
+            </p>
+          )}
         </div>
         <div className="px-2 flex gap-1">
-          <span className="font-semibold">faze_torger</span>
-          <p>Sjekk denne!</p>
+          <Link
+            href={`/auth/profile/${post.username}`}
+            className="font-semibold"
+          >
+            {post.username}
+          </Link>
+          <p>{post.caption}</p>
+        </div>
+        <form
+          action={postComment}
+          method="POST"
+          className="relative flex items-center rounded-lg border border-neutral-200"
+        >
+          <input type="hidden" name="postId" value={post.ID} />
+          <input type="hidden" name="username" value={user.username} />
+          <input
+            type="text"
+            className="w-full pl-3 pr-[45px] py-2 rounded-lg bg-neutral-100"
+            name="content"
+            placeholder="Add a comment..."
+          />
+          <button
+            type="submit"
+            className="absolute right-0 top-0 bottom-0 w-[40px] rounded-r-lg flex items-center justify-center px-3 text-lg text-gray-400"
+          >
+            <PiPaperPlaneRight className="-rotate-45 text-2xl" />
+          </button>
+        </form>
+        {post.comments.length === 0 && (
+          <p className="text-gray-500 text-sm px-2">No comments</p>
+        )}
+        {post.comments.length === 1 && (
+          <p className="text-gray-500 text-sm px-2">1 comment</p>
+        )}
+        {post.comments.length > 1 && (
+          <p className="text-gray-500 text-sm px-2">
+            {post.comments.length} comments
+          </p>
+        )}
+        <div className="max-h-[6rem] overflow-y-scroll">
+          <div className="flex flex-col h-fit px-2">
+            {post.comments.length > 0 &&
+              post.comments.map((comment) => {
+                return (
+                  <div key={comment.ID} className="flex gap-1">
+                    <span className="font-semibold">{comment.username}</span>
+                    <p>{comment.content}</p>
+                  </div>
+                );
+              })}
+          </div>
         </div>
       </section>
     </div>
--- client/src/components/user-preview.tsx
+++ client/src/components/user-preview.tsx
@@ -6,6 +6,7 @@ type UserPreviewProps = {
   url?: string;
   hideUrl?: boolean;
   hasStory?: boolean;
+  className?: string;
 };
 
 export function UserPreview({
@@ -13,46 +14,56 @@ export function UserPreview({
   url,
   hideUrl,
   hasStory,
+  className,
 }: UserPreviewProps) {
   url = url || `/auth/profile/${user.username}`;
+  className = className || "w-12 h-12 p-6 text-xl";
 
   return (
     <>
       {hideUrl ? (
-        <div>
+        <>
           {hasStory ? (
-            <div className="p-[2px] bg-gradient-to-bl from-rose-600 to-orange-400 h-fit w-fit rounded-full">
-              <div className="p-[1px] bg-white h-fit w-fit rounded-full">
-                <div className="p-6 h-14 w-14 flex items-center justify-center rounded-full bg-gray-300 hover:bg-gray-400 transition-colors duration-300">
-                  <span className="font-semibold capitalize text-xl">
+            <div className="p-[2px] bg-gradient-to-bl from-rose-600 to-orange-400 rounded-full">
+              <div className="p-[1px] bg-white rounded-full">
+                <div
+                  className={`${className} flex items-center justify-center rounded-full bg-gray-300 hover:bg-gray-400 transition-colors duration-300`}
+                >
+                  <span className="font-semibold capitalize">
                     {user.username[0]}
                   </span>
                 </div>
               </div>
             </div>
           ) : (
-            <div className="p-6 h-14 w-14 flex items-center justify-center rounded-full bg-gray-300">
-              <span className="font-semibold capitalize text-xl">
+            <div
+              className={`${className} flex items-center justify-center rounded-full bg-gray-300 hover:bg-gray-400 transition-colors duration-300`}
+            >
+              <span className="font-semibold capitalize">
                 {user.username[0]}
               </span>
             </div>
           )}
-        </div>
+        </>
       ) : (
         <Link href={url}>
           {hasStory ? (
-            <div className="p-[2px] bg-gradient-to-bl from-rose-600 to-orange-400 h-fit w-fit rounded-full">
-              <div className="p-[1px] bg-white h-fit w-fit rounded-full">
-                <div className="p-6 h-14 w-14 flex items-center justify-center rounded-full bg-gray-300 hover:bg-gray-400 transition-colors duration-300">
-                  <span className="font-semibold capitalize text-xl">
+            <div className="p-[2px] bg-gradient-to-bl from-rose-600 to-orange-400 rounded-full">
+              <div className="p-[1px] bg-white rounded-full">
+                <div
+                  className={`${className} flex items-center justify-center rounded-full bg-gray-300 hover:bg-gray-400 transition-colors duration-300`}
+                >
+                  <span className="font-semibold capitalize">
                     {user.username[0]}
                   </span>
                 </div>
               </div>
             </div>
           ) : (
-            <div className="p-6 h-14 w-14 flex items-center justify-center rounded-full bg-gray-300">
-              <span className="font-semibold capitalize text-xl">
+            <div
+              className={`${className} flex items-center justify-center rounded-full bg-gray-300 hover:bg-gray-400 transition-colors duration-300`}
+            >
+              <span className="font-semibold capitalize">
                 {user.username[0]}
               </span>
             </div>
